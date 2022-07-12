128164826810087dccce626104ca494d5cc33cf4
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-07-12 13:48:58 +0400

    added JS file, and fuction to display all existed expenses

---
+++ script.js
@@ -0,0 +1,23 @@
+const expansesContainer = document.querySelector('.expenses-container');
+const totalDisplay = document.getElementById('total');
+const API = 'http://localhost:3000/api/list/';
+
+async function fetchAPI() {
+    const url = await fetch("http://localhost:3000/api/list");
+    const res = await url.json();
+    res.forEach(element => {
+     getAllExpenses(element)
+    });
+}
+fetchAPI();
+
+// GET all data
+const getAllExpenses = (data) => {
+    expansesContainer.innerHTML += `<div class="list">
+    <p class="shop">Shop "${data.name}"</p>
+    <p class="date">${data.updatedAt}</p>
+    <p class="amount">$ ${data.price}</p>
+    <img id="${data.id}" onClick="updateInstanceById()" class="edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>
+    <img id="${data.id}" onClick="deleteExpanse()" class="delete" src="https://img.icons8.com/external-kosonicon-solid-kosonicon/48/000000/external-bin-cleaning-kosonicon-solid-kosonicon.png"/> 
+    </div>`
+}
