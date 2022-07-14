fb3b8d61d66ea47a6b10a8e1223be025e6524bab
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-07-14 13:08:29 +0400

    updated add method

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-use-before-define */
-const expansesContainer = document.querySelector('.expenses-container');
-async function fetchAPI() {
-  const url = await fetch('http://localhost:3000/api/list');
+const expensesContainer = document.querySelector('.expenses-container');
+const API = 'http://localhost:3000/api/list';
+
+const fetchAPI = async () => {
+  const url = await fetch(API);
   const res = await url.json();
   res.forEach((element) => {
-    getAllExpenses(element);
+    const listEl = renderExpense(element);
+    expensesContainer.append(listEl);
   });
 }
-fetchAPI();
-// GET all data
-const getAllExpenses = (data) => {
-  expansesContainer.innerHTML += `<div class="list">
-    <p class="shop">Shop "${data.name}"</p>
-    <p class="date">${data.updatedAt}</p>
-    <p class="amount">$ ${data.price}</p>
-    <img id="${data.id}" class="edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>
-    <img id="${data.id}" class="delete" src="https://img.icons8.com/external-kosonicon-solid-kosonicon/48/000000/external-bin-cleaning-kosonicon-solid-kosonicon.png"/> 
-    </div>`;
+
+const renderExpense = (data) => {
+  const { id, name, price, createdAt } = data;
+  const list = document.createElement('div');
+  list.setAttribute("class", "list");
+  list.innerHTML = `
+    <p class="shop">Shop "${name}"</p>
+    <p class="date">${createdAt}</p>
+    <p class="amount">$ ${price}</p>
+    <img class="edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>
+    <img class="delete" src="https://img.icons8.com/external-kosonicon-solid-kosonicon/48/000000/external-bin-cleaning-kosonicon-solid-kosonicon.png"/> 
+    `;
+
+  return list;
 };
 
 // POST new expenese
@@ -25,7 +30,7 @@ document.getElementById('add').addEventListener('click', async function addNewEx
   const shopValue = document.getElementById('shop-input').value;
   const priceValue = document.getElementById('amount-input').value;
   try {
-    const fetchResponse = await fetch('http://localhost:3000/api/list/', {
+    const fetchResponse = await fetch(API, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -37,13 +42,15 @@ document.getElementById('add').addEventListener('click', async function addNewEx
     });
     const res = await fetchResponse.json();
     if (Array.isArray(res)) {
-      expansesContainer.innerHTML = '';
-      res.forEach((element) => {
-        getAllExpenses(element);
-      });
+      expensesContainer.innerHTML = '';
+      fetchAPI();
       document.getElementById('shop-input').value = null;
       document.getElementById('amount-input').value = null;
 
     } else alert(res.answer);
   } catch (res) { /* catch error */ }
 });
+
+window.onload = () => {
+  fetchAPI();
+}
