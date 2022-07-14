92ca1b4b8a221db5d614b13f7849e0e6a9269074
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-07-14 12:49:10 +0400

    upgraded get method

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-use-before-define */
-const expansesContainer = document.querySelector('.expenses-container');
-async function fetchAPI() {
-  const url = await fetch('http://localhost:3000/api/list');
+const expensesContainer = document.querySelector('.expenses-container');
+const API = 'http://localhost:3000/api/list';
+
+const fetchAPI = async () => {
+  const url = await fetch(API);
   const res = await url.json();
   res.forEach((element) => {
-    getAllExpenses(element);
+    const listEl = getAllExpenses(element);
+    expensesContainer.append(listEl);
   });
 }
-fetchAPI();
-// GET all data
+
 const getAllExpenses = (data) => {
-  expansesContainer.innerHTML += `<div class="list">
-    <p class="shop">Shop "${data.name}"</p>
-    <p class="date">${data.updatedAt}</p>
-    <p class="amount">$ ${data.price}</p>
-    <img id="${data.id}" class="edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>
-    <img id="${data.id}" class="delete" src="https://img.icons8.com/external-kosonicon-solid-kosonicon/48/000000/external-bin-cleaning-kosonicon-solid-kosonicon.png"/> 
-    </div>`;
+  const { id, name, price, createdAt } = data;
+  const list = document.createElement('div');
+  list.setAttribute("class", "list");
+  list.innerHTML = `
+    <p class="shop">Shop "${name}"</p>
+    <p class="date">${createdAt}</p>
+    <p class="amount">$ ${price}</p>
+    <img class="edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>
+    <img class="delete" src="https://img.icons8.com/external-kosonicon-solid-kosonicon/48/000000/external-bin-cleaning-kosonicon-solid-kosonicon.png"/> 
+    `;
+
+  return list;
 };
+
+window.onload = () => {
+  fetchAPI();
+}
