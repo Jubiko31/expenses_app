47d6eec84bb69e086843f7f704e3f09e5146747a
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-07-16 00:41:10 +0400

    uploaded new feature to edit existed expense

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-return-assign */
 const expensesContainer = document.querySelector('.expenses-container');
 const API = 'http://localhost:3000/api/list';
 
@@ -26,6 +28,14 @@ const renderExpense = (data) => {
     deleteExpenseById(id)
   })
 
+  const editBtn = list.querySelector('.edit');
+  editBtn.addEventListener('click', () => {
+    const nameField = list.querySelector('.shop')
+    const name = nameField.innerText.substr(6).slice(0,-1);
+    const priceField = list.querySelector('.amount')
+    const price = priceField.innerText.substring(2);
+    updateInstanceById(id, name, price, nameField, priceField, editBtn)
+  })
   return list;
 };
 
@@ -106,3 +116,69 @@ const deleteExpenseById = async (id) => {
   return errorValue.innerHTML = error;
  }
 };
+
+const updateInstanceById = async (id, name, price, nameField, priceField, editBtn) => {
+  const errorValue = document.getElementById('error-display');
+  const successValue = document.getElementById('success');
+  const shopInputField = document.createElement('input');
+  const priceInputField = document.createElement('input');
+  const checkBtn = document.createElement('img');
+  nameField.parentNode.replaceChild(shopInputField, nameField);
+  priceField.parentNode.replaceChild(priceInputField, priceField);
+  editBtn.parentNode.replaceChild(checkBtn, editBtn)
+  checkBtn.src = 'https://img.icons8.com/color/48/000000/checked--v1.png';
+  shopInputField.value = name;
+  priceInputField.value = price;
+  shopInputField.classList.add('edit-input-name');
+  priceInputField.classList.add('edit-input-price');
+  let editedShopValue = shopInputField.value;
+  let editedPriceValue = priceInputField.value;
+  shopInputField.addEventListener('change', (e) => {
+    editedShopValue = e.target.value.trim();
+  });
+  priceInputField.addEventListener('change', (e) => {
+    editedPriceValue = e.target.value;
+  });
+  const update = async () => {
+  try {
+    if (!editedShopValue && !editedPriceValue) {
+      errorValue.style.display = 'block';
+      return errorValue.innerHTML = 'At least one input should be changed';
+    } 
+    if(editedPriceValue < 0 || isNaN(editedPriceValue)) {
+      errorValue.style.display = 'block';
+      return errorValue.innerHTML = 'Edited price must be a positive number.';
+    }
+    const URL = `${API}/${id}`;
+    expensesContainer.innerHTML = '';
+    const fetchedData = await fetch(URL, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: editedShopValue,
+        price: editedPriceValue
+      })
+    });
+    const response = await fetchedData.json();
+    if (response) {
+      response.forEach((element) => {
+        const listElement = renderExpense(element);
+        expensesContainer.append(listElement);
+        errorValue.style.display = 'none';
+        successValue.style.display = 'block';
+        successValue.innerText = 'One expense has been changed.';
+        setTimeout(() => {       
+          successValue.style.display = 'none';
+        }, 2000);
+      });
+    }
+   }
+   catch(error) {
+    errorValue.style.display = 'block';
+    return errorValue.innerHTML = error;
+   }
+  }
+  checkBtn.addEventListener('click', update);
+}
